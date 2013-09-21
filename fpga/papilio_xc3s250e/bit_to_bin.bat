22aa7a0403b5251c7eb1c848454461c8d4b99f49
ultra_embedded <ultra_embedded@0685d638-ab3d-4665-b31a-05a91e2f7b27>
2013-09-21 17:19:07 +0000

    Prepare for new release

--- fpga/papilio_xc3s250e/bit_to_bin.bat
+++
@@ -1,1 +0,0 @@
-promgen -u 0x0 project\fpga_top.bit -p bin -w -b -o fpga.bin
--- sw/bootloader/boot_flash.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BOOT_FLASH_H__
-#define __BOOT_FLASH_H__
-
-//-----------------------------------------------------------------
-// Prototypes:
-//-----------------------------------------------------------------
-int bootflash(unsigned long target);
-
-#endif // __BOOT_FLASH_H__
